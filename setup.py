caa791d4f6402ea563e2db40ecf6cd22dfff3539
niemeyer <Unknown>
2003-10-13 15:35:14 -0200

    Creating directory structure, and importing editmoin 1.0.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(name="editmoin",
+      version = "1.0",
+      description = "Edit Moin pages remotely with your preferred editor",
+      author = "Gustavo Niemeyer",
+      author_email = "niemeyer@conectiva.com",
+      url = "https://moin.conectiva.com.br/EditMoin",
+      license = "GPL",
+      long_description = 
+"""\
+This program allows you to edit moin (see http://moin.sourceforge.net)
+pages with your preferred editor. It means you can easily edit your
+pages, without the usual limitations of most web browsers' text areas.
+""",
+      scripts = ["editmoin"],
+      )
